be606de1826992c5611eca5d29af82c238415bcb
muqtar <muqtarabdiaziz8@gmail.com>
2023-11-23 22:34:22 +0300

    comp

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,53 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if username.upper() =="ADMIN" and password=="12345":
+        message="Access granted"
+    else:
+        message="Access denied"
+    return message
+
+
+    
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature<40:
+        message="It's brisk out there!"
+    elif temperature >39 and temperature<=65:
+        message="It's a little chilly out there!"
+    elif temperature >85:
+        message="It's too dang hot out there!"
+    else:
+        message= "It's perfect out there!"
+    return message
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0 and num % 5 == 0:
+        message = "FizzBuzz"
+    elif num % 3 == 0:
+        message = "Fizz"
+    elif num % 5 == 0:
+        message = "Buzz"
+    else:
+        message = num
+    return message
 
 def calculator(operation, num1, num2):
+
     # your code here
-    pass
+    if operation=="+":
+        result=num1+num2
+    elif operation=="/":
+        result=num1/num2
+    elif operation =="*":
+        result= num1*num2
+    elif operation =="-":
+        result=num1-num2
+    else:
+        print("Invalid operation!")
+        return None
+    return result
+    
